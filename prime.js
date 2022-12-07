3e3d80fa8f2c11eccc00e36426225f2035c9aa03
akshayaravindran <akshayaravindran1996@gmail.com>
2022-12-07 15:26:18 +0530

    created prime file

---
+++ prime.js
@@ -0,0 +1,22 @@
+function checkPrime(num){
+let count=0;
+for(let i=1; i<=num; i++){
+if(num%i==1){
+count++;
+
+}
+}
+if(count==2){
+return true;
+}
+return false;
+
+}
+
+let ans= checkPrime(11);
+
+if(ans==true) {
+console.log("prime")
+}else{
+console.log("not prime")
+}
